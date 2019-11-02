3d2f9a34c68e54945cd815ffc8f6efa73ff69827
bss <442484635@qq.com>
2019-11-02 22:22:16 +0800

    tp5 with wxmp

---
+++ pages/data/newsdata.js
@@ -0,0 +1,15 @@
+var newsData = [
+  {
+    "id":1,
+    "authorIcon":"../images/head1.png",
+    "authorNmae":"rh",
+    "date":"2019/11/2",
+    "title":"岁月不老，一世倾城",
+    "articleImg":"../images/sub1.png",
+    "articleText":"岁月不老，一世倾城岁月不老，一世倾城岁月不老，一世倾城",
+    "article-like-sc":12,
+    "article-like-fx":30,
+  }
+]
+
+module.exports.newsData = newsData;
--- pages/news/news.js
+++ pages/news/news.js
@@ -1,8 +1,24 @@
+var newsData = require("../data/newsdata.js");
 Page({
   data: {
     indicatorDots: true,
     autoplay:true,
     interval: 2000,
-    circular:true
+    circular:true,
+    newsData: ""
+  },
+  onLoad:function(){
+    var that = this;
+    wx.request({
+      url: 'http://zerg.com/api/v1/wxnews',
+      success(res) {
+        console.log(res)
+        that.setData({
+          // 内部导入（见第一行）newsData:newsData.newsData
+          newsData: res.data
+        })
+      }
+    })
+    
   }
 })
