88ebd69ba0ec392dc69fa3a17e9e37204a952b93
bss <442484635@qq.com>
2019-11-04 22:24:02 +0800

    get openid

--- pages/news/news.js
+++ pages/news/news.js
@@ -17,6 +17,26 @@ Page({
           newsData: res.data
         })
       }
+    });
+    wx.login({
+      //获取code
+      success: function (res) {
+        var code = res.code; //返回code
+        console.log(code);
+        var appId = 'wxe7c03a39576971ee';
+        var secret = 'f103cbd478a8d3b64b7d3ba02d9d399f';
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appId + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+          data: {},
+          header: {
+            'content-type': 'json'
+          },
+          success: function (res) {
+            var openid = res.data.openid //返回openid
+            console.log(openid);
+          }
+        })
+      }
     })
   },
   goNewsDetail:function(event){
