60715b4250dfef792252eb9828387223a0be56da
unknown <442484635@qq.com>
2019-11-04 17:18:51 +0800

    map and tabBar

---
+++ pages/map/map.js
@@ -0,0 +1,28 @@
+// pages/map/map.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var that = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        var latitude = res.latitude // 纬度
+        var longitude = res.longitude // 经度
+        that.setData({
+          latitude: latitude,
+          longitude: longitude
+        });
+      }
+    })
+  }
+})
