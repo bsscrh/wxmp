1f95edff96adfaffcebb816a4a0793bea2aae6d1
unknown <442484635@qq.com>
2019-12-10 17:07:17 +0800

    11-3 获取banner company

--- pages/home/home-model.js
+++ pages/home/home-model.js
@@ -1,4 +1,13 @@
 class Home {
-
+  getBannerData(id) {
+    wx.request({
+      url: 'http://zerg.com/api/v1/banner/'+id,
+      method:'GET',
+      success:function(res){
+        console.log(res);
+      }
+    })
+  }
 }
+
 export { Home };
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,17 +1,15 @@
 import { Home } from 'home-model.js';
+var home = new Home();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this._loadData();
+  },
+  _loadData: function () {
+    var id = 1;
+    // 获得bannar信息
+    var data = home.getBannerData(id);
   }
 })
