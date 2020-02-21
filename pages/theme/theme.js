506abb95f0176b5e2bced5386981be33a4c00b51
bss <442484635@qq.com>
2020-02-21 12:35:26 +0800

    11-15 显示数据

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,18 +1,39 @@
-// pages/theme/theme.js
+import { Theme } from 'theme-model.js';
+var theme = new Theme(); //实例化  主题列表对象
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    loadingHidden: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (option) {
+    this.data.titleName = option.name;
+    this.data.id = option.id;
+    wx.setNavigationBarTitle({
+      title: option.name
+    });
+    this._loadData();
+    
+  },
 
+  /*加载所有数据*/
+  _loadData: function (callback) {
+    var that = this;
+    /*获取单品列表信息*/
+    theme.getProductorData(this.data.id, (data) => {
+      that.setData({
+        themeInfo: data,
+        loadingHidden: true
+      });
+      callback && callback();
+    });
+    
   },
 
   /**
